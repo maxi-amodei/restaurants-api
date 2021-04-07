72df65f46ea04969156877776ed82518ea59a47d
Maxi Amodei <maxi_amodei@hotmail.com>
2021-04-07 12:18:09 -0300

    Addind create and delete actions

--- app/policies/restaurant_policy.rb
+++ app/policies/restaurant_policy.rb
@@ -8,4 +8,18 @@ class RestaurantPolicy < ApplicationPolicy
   def show?
     true
   end
+
+  def update?
+    # Only restaurant Owners can Update
+  record.user == user
+  end
+
+  def create?
+    # Any logged in user can create a restaurant
+    !user.nil?
+  end
+
+  def destroy?
+    record.user == user
+  end
 end
