8ff25e96cace519fe58554dea9d27400bee5c967
Maxi Amodei <maxi_amodei@hotmail.com>
2021-04-13 20:27:14 -0300

    Including helper module

--- spec/support/models/restaurant_helper.rb
+++ spec/support/models/restaurant_helper.rb
@@ -1,5 +1,6 @@
-
+module RestaurantHelper
   def create_restaurant(email, password, name, address)
     user = User.create!(email: email, password: password)
     Restaurant.create!(name: name, address: address, user: user)
   end
+end
