14643719d745992566fb0a756b2e5ae3ebea13eb
Maxi Amodei <maxi_amodei@hotmail.com>
2021-04-13 20:02:30 -0300

    Testing delete method with 2 contexts

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::RestaurantsController < Api::V1::BaseController
 
   def index
     @restaurants = policy_scope(Restaurant)
-    render :json => @restaurants
+    
   end
 
   def show
--- spec/requests/restaurants_spec.rb
+++ spec/requests/restaurants_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe 'Restaurants requests' do
 
       expect(response.status).to eq(200)
     end
-
     it "returns the created restaurant" do
       user = User.create!(email:"max@amodei", password:"123456")
       Restaurant.create!(name:"Mcdonalds", address:"Blvrd streeet 1234", user: user)
@@ -19,6 +18,30 @@ RSpec.describe 'Restaurants requests' do
       expect(array[0]["name"]).to eq("Mcdonalds")
     end
   end
+
+  describe " DELETE /api/v1/restaurants/:id" do
+    context "the user has authentication token" do
+      it "returns 204 when user is authorized" do
+        user = User.create!(email:"max@amodei", password:"123456")
+        restaurant = Restaurant.create!(name:"Mcdonalds", address:"Blvrd streeet 1234", user: user)
+        
+        delete "/api/v1/restaurants/#{restaurant.id}",  headers: { 'X-User-Email': user.email, 'X-User-Token': user.authentication_token }
+        
+        expect(response.status).to eq(204)
+      end
+    end
+    context "the user has authentication token" do
+      it "returns 401 when user is not authorized" do
+        user = User.create!(email:"max@amodei", password:"123456")
+        restaurant = Restaurant.create!(name:"Mcdonalds", address:"Blvrd streeet 1234", user: user)
+        
+       delete "/api/v1/restaurants/#{restaurant.id}"
+  
+        expect(response.status).to eq(401)
+      end
+    end
+  end
+
 end
 
 
